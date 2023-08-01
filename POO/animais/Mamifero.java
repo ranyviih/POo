bbf3a0d94e53a42f9188096a70abd99e49a00937
ranyviih <124800291+ranyviih@users.noreply.github.com>
2023-07-31 23:41:35 -0300

    Mamifero.java

--- POO/animais/Mamifero.java
+++ POO/animais/Mamifero.java
@@ -1,12 +1,13 @@
 package animais;
+//Classe mamifero
 public class Mamifero{
-    String nome;
-    int idade = 0;
-
+    String nome; //atribulto
+    int idade = 0; //atribulto
+//metodo emitir som
     public void emitirSom() {
         System.out.println("Emitir som...");
     }
-
+//metodo mover
     public void mover() {
          System.out.println("Mover...");
     }   
