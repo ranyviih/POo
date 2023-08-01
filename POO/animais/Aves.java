f16fb30cc49d58c65e70c6243dbdcb650894a4d9
ranyviih <124800291+ranyviih@users.noreply.github.com>
2023-07-31 23:35:33 -0300

    Aves.java

--- POO/animais/Aves.java
+++ POO/animais/Aves.java
@@ -1,13 +1,13 @@
 package animais;
-
+//Classe Aves
 public class Aves {
     String nome;
     int idade;
-
+// metodo emitir som
     public void emitirSom() {
         System.out.println("Emitindo som");
     }
-
+//metodo voar
     public void voar() {
         System.out.println("Voando");
     }
