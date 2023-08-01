a6ce3c19b54847abeb89a0c5798bc03b8de402a5
ranyviih <124800291+ranyviih@users.noreply.github.com>
2023-07-31 23:37:26 -0300

    Cachorro.java

--- POO/animais/Cachorro.java
+++ POO/animais/Cachorro.java
@@ -1,14 +1,14 @@
 package animais;
-
+//Classe Cachorro herdada de Mamifero
 public class Cachorro extends Mamifero {
     String raca;
-
+//Construtor
     Cachorro(String nome, int idade, String raca) {
         this.nome = nome;
         this.idade = idade;
         this.raca = nome;
     }
-
+//metodo abanar rabo
     public void abanarRabo() {
         System.out.println("Abanando rabo");
     }
