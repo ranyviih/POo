be92b2eb9166f441712b1a334c9c81a90e50c686
ranyviih <124800291+ranyviih@users.noreply.github.com>
2023-07-31 23:44:21 -0300

    Passaros.java

--- POO/animais/Passaros.java
+++ POO/animais/Passaros.java
@@ -1,14 +1,16 @@
 package animais;
+//Classe passaros herdada da classe aves
 public class Passaros extends Aves{
     String nome;
     int idade = 0;
     String cor;
-
+//construtor
     Passaros(String nome, int idade, String cor) {
         this.nome = nome;
         this.idade = idade;
         this.cor = cor;
     }
+    //metodo constrir ninho
     public void construirNinho() {
         System.out.println("Construindo ninho");  
     }
